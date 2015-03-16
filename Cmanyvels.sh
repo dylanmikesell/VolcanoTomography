a90336404892feb8b7b5b5d46a50c45be262f25f
Dylan Mikesell <mikesell@mit.edu>
2015-03-16 15:58:56 -0400

    fixing

--- Cmanyvels.sh
+++ Cmanyvels.sh
@@ -6,15 +6,12 @@ snum=81
 snumiter=1
 while [ $snumiter -le $snum ]
 do
+
 echo $snumiter
 cp runFiles/pronto$snumiter.run pronto.run
-
 cp obsData/data$snumiter.obs data.obs
-
 pronto.com > outFiles/pronto$snumiter.out
-
 cp vel.final velData/vel$snumiter.final
-
 cp ray.final rayData/ray$snumiter.final
 
 snumiter=`bc -l <<END
